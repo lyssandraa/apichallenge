3ef81cf3500294ddf2965556b0840ea8089e42e6
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-24 20:02:16 +0100

    imported modal into app. fetched new data to display

--- apichallenge/src/App.jsx
+++ apichallenge/src/App.jsx
@@ -24,4 +24,51 @@ const App = () => {
       setErrorMsg(error.message);
     }
   };
+  const handleOpenModal = (book) => {
+    setSelectedBook(book);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedBook(null);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {errorMsg !== "" ? (
+        <p>{errorMsg}</p>
+      ) : (
+        <>
+          <div className="wrapper">
+            {bookData.map((book, index) => (
+              <div key={index} className="content">
+                <img
+                  src={book.cover}
+                  alt={book.title}
+                  onClick={() => handleOpenModal(book)}
+                />
+              </div>
+            ))}
+          </div>
+          <Modal show={showModal} onClose={handleCloseModal}>
+            {selectedBook && (
+              <>
+                <img src={selectedBook.cover} alt={book.title} />
+                <h2>{selectedBook.title}</h2>
+                <p>{selectedBook.release_date}</p>
+                <p>{selectedBook.summary}</p>
+              </>
+            )}
+          </Modal>
+        </>
+      )}
+    </>
+  );
 };
+
+export default App;
