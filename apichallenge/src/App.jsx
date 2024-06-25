0945c07bce21b80fba078172f6b30b5c7adea922
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-25 14:09:36 +0100

    changed background image and adjusted css

--- apichallenge/src/App.jsx
+++ apichallenge/src/App.jsx
@@ -58,6 +58,7 @@ const App = () => {
           <Modal show={showModal} onClose={handleCloseModal}>
             {selectedBook && (
               <>
+                {/* <img src={book.cover} alt={book.title} /> */}
                 <h2>{selectedBook.title}</h2>
                 <p>{selectedBook.release_date}</p>
                 <p>{selectedBook.summary}</p>
